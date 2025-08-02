44f80c1009f27b36bf40e08b6917ff24ccf3be0f
Raywall Malheiros <raywall.malheiros@gmail.com>
2025-08-02 11:27:54 -0300

    update

--- examples/how-to-use.go
+++ examples/how-to-use.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/raywall/pseudonymization-lgpd-tools"
+	"github.com/raywall/pseudonymization-lgpd-tools/utils"
 )
 
 func ExampleService_Pseudonymize() {
@@ -43,6 +44,26 @@ func ExampleService_Pseudonymize() {
 	// Original value: 12345678901
 }
 
+func ExampleCPFFunctions() {
+	// Validate a CPF
+	valid := utils.IsValidCPF("529.982.247-25")
+	fmt.Printf("Is valid CPF: %v\n", valid)
+
+	// Generate a synthetic CPF
+	synthetic, err := utils.GenerateSyntheticCPF()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Synthetic CPF: %s\n", synthetic)
+	fmt.Printf("Is valid: %v\n", utils.IsValidCPF(synthetic))
+
+	// Output:
+	// Is valid CPF: true
+	// Synthetic CPF: 999.XXX.XXX-XX
+	// Is valid: true
+}
+
 func main() {
 	ExampleService_Pseudonymize()
+	ExampleCPFFunctions()
 }
